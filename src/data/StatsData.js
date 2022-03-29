06714f762298aadc7a78fa13796cb15d8542e49a
FelipeloDev <felipe.londono@pragma.com.co>
2022-03-29 10:46:48 -0500

    first commit

---
+++ src/data/StatsData.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { GiEarthAmerica } from "react-icons/gi"
+import { MdAirplanemodeActive, MdTimer } from "react-icons/md"
+import { FaMoneyCheck } from "react-icons/fa"
+
+export const StatsData = [
+  {
+    icon: <GiEarthAmerica />,
+    title: "Over 100 Destinations",
+    desc: "Travel to over 100 unique places",
+  },
+  {
+    icon: <MdAirplanemodeActive />,
+    title: "1 Million trips made",
+    desc: "over 1 Million trips completed last year",
+  },
+  {
+    icon: <MdTimer />,
+    title: "Fastest support",
+    desc: "Access our support team 24/7",
+  },
+  {
+    icon: <FaMoneyCheck />,
+    title: "Best deals",
+    desc: "We offer the best prices",
+  },
+]
